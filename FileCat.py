d217c6a57ab6a0d8c89ed4a54d187a19d378305a
Wiki Updater <semina054@gmail.com>
2020-08-16 13:53:43 +0200

    Update files

---
+++ FileCat.py
@@ -0,0 +1,57 @@
+import os, json, time, tkinter
+from tkinter import *
+
+# Выгрузка config.json
+with open('config.json') as cnfFILE:
+	config_data = json.load(cnfFILE)
+
+# Выгрузка языка
+with open('{0}\\languages\\{1}'.format(os.getcwd(), config_data["language"])) as LANGFILE:
+	language_data = json.load(LANGFILE)
+
+root = Tk() # Создание окна
+root.geometry('700x600') # Размер окна
+root.resizable(width = False, height = False) # Блокировка размера окна, чтобы его нельзя было изменить
+root.title("{0}".format(language_data["name_window"])) # Имя окна
+
+# Создание объектов
+language_change_B = Button(root, text = "{0}".format(language_data['name_lang']))
+
+# Логика
+def language_change_click(event):
+	list_languages = os.listdir(path = "{0}\\languages".format((os.getcwd())))
+
+	id_lang_list = 0
+	while True:
+		if config_data["language"] == list_languages[id_lang_list]:
+			break
+		id_lang_list += 1
+	id_lang_list += 1
+	if id_lang_list == len(list_languages):
+		id_lang_list = 0
+	loading_text_language(event, id_lang_list, list_languages, config_data)
+
+def loading_text_language(event, id_lang, list_langs, config_data):
+	id_lang_list = id_lang
+	list_languages = list_langs
+
+	config_data["language"] = list_languages[id_lang_list]
+	with open('config.json', 'w') as cnfFILE:
+		json.dump(config_data, cnfFILE)
+
+	with open('config.json') as cnfFILE:
+		config_data = json.load(cnfFILE)
+
+	with open('{0}\\languages\\{1}'.format(os.getcwd(), config_data["language"])) as LANGFILE:
+		language_data = json.load(LANGFILE)
+
+	root.quit()
+
+# Параметры объекта и их привязка к логике
+language_change_B.bind('<Button-1>', language_change_click)
+
+# Выгрузка объектов на экран
+language_change_B.pack()
+language_change_B.place(x = 5, y = 5)
+
+root.mainloop()
