d22b393b9ecbebee46d1b7b87b46f545b5f14194
romanin-rf <semina054@gmail.com>
2021-01-03 17:07:06 +0200

    Add system update element

--- FileCat.py
+++ FileCat.py
@@ -1,4 +1,9 @@
-import os, json, time, tkinter, math, tkinter.ttk as ttk
+import os
+import json
+import time
+import math
+import tkinter
+import tkinter.ttk as ttk
 from tkinter import *
 
 def calculate_whole_percentage(max_var, var, percent):
@@ -25,14 +30,29 @@ root.geometry('700x600') # Размер окна
 root.resizable(width = False, height = False) # Блокировка размера окна, чтобы его нельзя было изменить
 root.title("{0}".format(language_data["name_window"])) # Имя окна
 
+# Обрабочик
+def handler_progress(userdata):
+	if userdata["save"]["start-value"] <= userdata["save"]["you-user"]:
+		userdata["save"]["multiplier-start-value"] += userdata["setting"]["speed-rise-multiplier-start-value"]
+		userdata["save"]["money"] += userdata["setting"]["start-money-for-lvl"]
+	max_start_value = userdata["save"]["start-value"] * userdata["save"]["multiplier-start-value"]
+	progress_percentage = calculate_whole_percentage(max_var = max_start_value, var = userdata["save"]["you-user"], percent = 100)
+	with open('user_data.json', "w") as user_data_file:
+		json.dump(userdata, user_data_file)
+	return max_start_value, userdata["save"]["you-user"], progress_percentage
+
+max_start_value_progress, value_progress_user, percentage_progress_user = handler_progress(userdata = usr_data)
+
 # Создание объектов
 language_change_B = Button(root, text = "{0}".format(language_data['name_lang']))
 version_text_var = Label(root, text = "{0}: {1}".format(language_data["text_window"]["text_version"], config_data["version"]))
 bit_progressbar = ttk.Progressbar(root, length = 570)
-bit_progressbar_value_text = Label(root, text = "{0}: *\\* (* %)".format(language_data["text_window"]["text_progress"]))
+bit_progressbar["value"] = percentage_progress_user
+bit_progressbar_value_text = Label(root, text = "{0}: {1} {4} \\{2} {4} ({3} %)".format(language_data["text_window"]["text_progress"], value_progress_user, max_start_value_progress, percentage_progress_user, language_data["text_window"]["text_bites"]))
 money_vaule_text = Label(root, text = "{0}: {1}".format(language_data["text_window"]["text_money"], usr_data["save"]["money"]))
+button_feed_the_cat = Button(root, text = "{0}".format(language_data["button_text_window"]["feed_the_cat"]))
 
-# Логика
+# Логикa
 def language_change_click(event):
 	list_languages = os.listdir(path = "{0}\\languages".format((os.getcwd())))
 
@@ -81,3 +101,5 @@ bit_progressbar_value_text.place(x = 100, y = 30)
 money_vaule_text.place(x = 100, y = 50)
 
 root.mainloop()
+with open('user_data.json', "w") as user_data_file:
+	json.dump(userdata, user_data_file)
--- languages/load.py
+++ languages/load.py
@@ -1,13 +1,17 @@
 import json, os
 
 data = 	{
-			"name_lang": 	"Русский", 
-			"name_window": 	"Файловый Кот", 
-			"text_window": 	{
-								"text_version": "Версия",
-								"text_progress": "Прогресс",
-								"text_money": "Монет"
-							}
+			"name_lang": 			"Русский", 
+			"name_window": 			"Файловый Кот", 
+			"text_window": 			{
+										"text_version":		"Версия",
+										"text_progress":	"Прогресс",
+										"text_money":		"Монет",
+										"text_bites":		"Байт(ов)"
+									},
+			"button_text_window":	{
+										"feed_the_cat":		"Покормить"
+									}
 		}
 
 with open('RU.json', 'w') as file:
